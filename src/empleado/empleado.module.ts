eaeef289c55f37ea86c9585a1d490f18dcd5b374
Rene <rene.lozano@tecsup.edu.pe>
2025-05-23 12:23:19 -0500

    delete project

--- src/empleado/empleado.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { EmpleadoService } from './empleado.service';
-import { EmpleadoController } from './empleado.controller';
-
-@Module({
-  controllers: [EmpleadoController],
-  providers: [EmpleadoService],
-})
-export class EmpleadoModule {}
